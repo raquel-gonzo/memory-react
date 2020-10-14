e3164684769fe30f5c2f7f6584ac516664f8e264
raquel-gonzo <gonzalez.rachel.c@gmail.com>
2020-10-13 22:30:41 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Card from "./components/card";
+import GameBoard from './components/game-board';
 
 function App() {
+  var attempts = 0;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>match the sugar skulls!</h1>
+
+      <h2>attempts: {attempts}</h2>
+
+      <button>new game</button>
     </div>
   );
 }
---
+++ src/components/game-board.js
@@ -0,0 +1,16 @@
+import React, {useState} from 'react';
+import Card from './card';
+
+const GameBoard = () => {
+
+    
+  let hasFlippedCard = false;
+  let lockBoard = false;
+  let firstCard, secondCard;
+
+    return(
+        <div></div>
+    )
+}
+
+export default GameBoard;
