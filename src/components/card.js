c7bd704ab886abb8233fe49d748b83a6d79be40d
raquel-gonzo <gonzalez.rachel.c@gmail.com>
2020-10-17 21:41:57 -0500

    useState hook used on card component for click handler.

--- src/components/card.js
+++ src/components/card.js
@@ -1,27 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { Image, Transformation } from "cloudinary-react";
 
 const Card = (props) => {
   // each card should have a front-face and a back-face.
+
+  // isFlipped
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const onClick = () => {
+    setIsFlipped(true);
+  }
+
   return (
     <div className="card">
-      {/* front */}
-      <Image
-        cloudName="raquel-gonzo"
-        className={props.className}
-        publicId={props.publicId}
-      >
-        <Transformation width="150" height="210" crop="scale" />
-      </Image>
-
-      {/* back */}
-      <Image 
-        cloudName="raquel-gonzo"
-        publicId="memory-react/cards-icon.png"
-        className="back-face"
+      {isFlipped ? (
+        // front
+        <Image
+          cloudName="raquel-gonzo"
+          className={props.className}
+          publicId={props.publicId}
         >
-        <Transformation width="150" height="210" crop="scale" />
-      </Image>
+          <Transformation width="150" height="210" crop="scale" />
+        </Image>
+      ) : (
+        // back
+        <Image
+          cloudName="raquel-gonzo"
+          publicId="memory-react/cards-icon.png"
+          className="back-face"
+          onClick={onClick}
+        >
+          <Transformation width="150" height="210" crop="scale" />
+        </Image>
+      )}
     </div>
   );
 };
